c70fcfe2912bed495a1ab8ee81091884add15b12
Gray Gilmore <graygilmore@gmail.com>
2015-10-18 21:58:02 -0400

    Add basic modeling for products

---
+++ spec/models/product_spec.rb
@@ -0,0 +1,16 @@
+require "rails_helper"
+
+RSpec.describe Product, type: :model do
+  it { should validate_presence_of :name }
+  it { should validate_presence_of :price }
+
+  it do
+    # TODO
+    # Rspec 3.0.0 broke how numericality was tested. This test
+    # should be turned back on when this is fixed.
+    pending("Waiting for Rspec to fix numericality tests")
+    should validate_numericality_of(:price).
+      is_greater_than_or_equal_to(0).
+      only_integer
+  end
+end
