47287ce4c7fc99dee76240383dc441bf8eb34430
Gray Gilmore <graygilmore@gmail.com>
2015-11-07 15:53:39 -0800

    Fix price_cents validation test

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,6 @@
 class Product < ActiveRecord::Base
   validates :name, :price_cents, presence: true
   validates :price_cents, numericality: {
-    greater_than_or_equal_to: 0,
-    only_integer: true
+    greater_than_or_equal_to: 0
   }
 end
