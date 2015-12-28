f745c714246bbac2fa83c6e98987790d8f345dd3
Gray Gilmore <graygilmore@gmail.com>
2015-12-27 21:20:59 -0500

    Remove password_digest column from users
    Authlogic does not use this so we no longer need it.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151227233255) do
+ActiveRecord::Schema.define(version: 20151228022024) do
 
   create_table "products", force: :cascade do |t|
     t.string   "name"
@@ -26,7 +26,6 @@ ActiveRecord::Schema.define(version: 20151227233255) do
     t.string   "email",                             null: false
     t.datetime "created_at",                        null: false
     t.datetime "updated_at",                        null: false
-    t.string   "password_digest"
     t.boolean  "admin",             default: false
     t.string   "crypted_password"
     t.string   "password_salt"
